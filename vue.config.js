fcb4228ad0a224b20b48fc0140b2c5a90198129b
Radames Ajna <radamajna@gmail.com>
2019-02-15 11:13:16 -0800

    html tile comes from vueconfig file

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,23 @@
 module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'YR Media - Dress Code Interactive',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  },
   css: {
     sourceMap: true
   },
-
   publicPath: '/dress-code-interactive/',
   outputDir: 'dist',
   assetsDir: 'src/assets',
