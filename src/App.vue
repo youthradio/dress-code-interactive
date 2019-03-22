c4fe56c8d0aa50d8d91f941e662da470f663faa0
Radames Ajna <radamajna@gmail.com>
2019-03-22 16:01:01 -0700

    options button

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <div>
     <MHeader />
     <div class="row-flex-column ">
-      <MSchoolsHeader class="schools-header"/>
-      <MOutfits class="order"/>
-      <MSchoolsResult v-if="hasVoted"/>
-      <div class="row-flex-column">
+      <MSchoolsHeader class="schools-header" />
+      <MOutfits class="order" />
+      <MSchoolsResult v-if="hasVoted" />
+      <div class="row-flex-column" v-else>
         <div class="message">
-          <h2> Do you think the student will get dress coded? </h2>
+          <h2>Do you think the student will get dress coded?</h2>
         </div>
         <div class="row-flex center">
-          <button class="button-style"> Yes </button>
-          <button class="button-style"> No </button>
+          <button @click="vote('YES')" class="button-style">Yes</button>
+          <button @click="vote('NO')" class="button-style">No</button>
         </div>
       </div>
     </div>
@@ -19,15 +19,15 @@
 </template>
 
 <script>
-import MainComponent from './components/MainComponent.vue'
-import MSchoolsHeader from './components/MSchoolsHeader.vue'
-import MHeader from './components/MHeader.vue'
-import MOutfits from './components/MOutfits.vue'
-import MSchoolsResult from './components/MSchoolsResult.vue'
-import ResizeObserver from 'resize-observer-polyfill';
+import MainComponent from "./components/MainComponent.vue";
+import MSchoolsHeader from "./components/MSchoolsHeader.vue";
+import MHeader from "./components/MHeader.vue";
+import MOutfits from "./components/MOutfits.vue";
+import MSchoolsResult from "./components/MSchoolsResult.vue";
+import ResizeObserver from "resize-observer-polyfill";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     MainComponent,
     MSchoolsHeader,
@@ -37,65 +37,62 @@ export default {
   },
   data() {
     return {
-      hasVoted: false,
-    }
+      hasVoted: false
+    };
   },
-  computed: {
-
-  },
+  computed: {},
   created() {
     // load data
-    this.$store.dispatch('fetchData');
+    this.$store.dispatch("fetchData");
   },
   mounted() {
     const elementRoot = this.$el;
     const resizeObserver = new ResizeObserver(entries => {
       for (const entry of entries) {
-        const {
-          height
-        } = entry.contentRect;
-        const elementHeight = 'elementHeight:' + height;
+        const { height } = entry.contentRect;
+        const elementHeight = "elementHeight:" + height;
         // console.log(elementHeight);
-        parent.postMessage(elementHeight, '*');
+        parent.postMessage(elementHeight, "*");
       }
     });
     resizeObserver.observe(elementRoot);
   },
   methods: {
-
-  },
-}
+    vote(option) {
+      this.hasVoted = true;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 @import "~@/styles/custom";
-@import '~@/styles/mixins';
+@import "~@/styles/mixins";
 
 .grid-row {
   display: grid;
-  grid-template-columns: repeat( auto-fit, minmax(400px, 1fr) );
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
   grid-auto-flow: dense;
 }
 
 .center {
   justify-content: center;
 }
-.schools-header{
+.schools-header {
   // margin: auto;
-  @include breakpoint("small"){
+  @include breakpoint("small") {
     max-width: 100%;
   }
-  @include breakpoint("large"){
+  @include breakpoint("large") {
     max-width: 50%;
   }
-
 }
 .margin {
   width: 100%;
-  @include breakpoint("small"){
+  @include breakpoint("small") {
     max-width: 70%;
   }
-  @include breakpoint("large"){
+  @include breakpoint("large") {
     max-width: 50%;
   }
 }
@@ -104,10 +101,10 @@ export default {
   background-color: $white;
   border: 3px solid $dark;
   border-radius: 10px;
-  padding: .5rem .5rem;
+  padding: 0.5rem 0.5rem;
   font-size: 1.5rem !important;
-  font-family: 'Assistant', sans-serif;
-	font-weight: 600;
+  font-family: "Assistant", sans-serif;
+  font-weight: 600;
   box-shadow: 0.3rem 0.3rem $sky-blue;
   margin: 0.6rem;
 }
